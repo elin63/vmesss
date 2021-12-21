46164ca62118688f01bd7b03617311612d8cf44d
elin63 <88765647+elin63@users.noreply.github.com>
2021-12-21 21:58:32 +0800

    Update a.js

--- a.js
+++ a.js
@@ -102,7 +102,7 @@ function getzqqdbody() {
                 $.msg($.name, "获取第" + bodys.length + "个签到请求: 成功🎉", ``)
             }
         } else {
-            $.setdata(bodyVal, 'zqqdbody');
+            $.setdata(bodyVal, '中青签到body');
             $.log(`${$.name}获取签到: 成功, zqqdbodys: ${bodyVal}`);
             $.msg($.name, `获取第一个签到请求: 成功🎉`, ``)
         }
